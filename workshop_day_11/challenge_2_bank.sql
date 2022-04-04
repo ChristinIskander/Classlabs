c54c2dd7feb5ead59e9c69b58972c6d822babb5d
Christin Iskander <christine.r.iskander@gmail.com>
2022-04-04 18:03:39 +0200

    Create challenge_2_bank.sql

---
+++ workshop_day_11/challenge_2_bank.sql
@@ -0,0 +1,57 @@
+use bank;
+--1
+select * from client;
+select client_id from client
+where district_id =1
+limit 5
+--2
+select client_id from client
+where district_id =72
+order by client_id desc
+limit 1
+--3
+select * from loan
+select amount from loan
+order by amount asc
+limit 3
+--4
+select distinct status from loan 
+order by status asc
+--5
+select * from loan
+select loan_id from loan 
+order by payments desc
+limit 1
+--6
+select account_id, amount from loan
+order by account_id asc
+limit 5
+--7
+select account_id from loan
+where duration = 60
+order by amount asc
+limit 5
+--8
+select distinct k_symbol from `order`
+order by k_symbol asc
+--9
+select order_id from `order`
+where account_id=34
+--10
+select distinct account_id from `order`
+where order_id between 29540 and 29560 
+--11
+select amount from `order`
+where account_to = 30067122
+--12
+select trans_id, date, type, amount  from trans
+where account_id = 793
+order by date desc
+limit 10
+--13
+select district_id, count(client_id) from client
+where district_id < 10
+group by district_id
+order by district_id asc
+--14
+select type, count ( from card
