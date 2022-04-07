11016a913198cef1ed102d56c90ac245f6023d6d
Christin Iskander <christine.r.iskander@gmail.com>
2022-04-07 17:48:05 +0200

    Delete challenge sql_intro.sql

--- workshop_day_11/challenge sql_intro.sql
+++
@@ -1,8 +0,0 @@
-use sakila;
-show tables from sakila;
-show columns from address
-select title from film
-select name as film_language from language
-select * from store
-select * from staff
-select first_name from staff
